6d96039a2d1f9c42979ad63feda5c25c1a72d252
isaacoduh <ufuomaoduh@gmail.com>
2019-09-29 18:08:05 +0100

    Modify error handler to show json response

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -46,6 +47,19 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        /**
+         * This will replace our 404 response with a JSON response
+         * If you’re using Laravel to serve other pages, you have to edit the code to work 
+         * with the Accept header, otherwise 404 errors from regular requests will 
+         * return a JSON as well.
+         * In this case, the API requests will need the header Accept: application/json.
+         */
+        if($exception instanceof ModelNotFoundException && $request->wantsJson()){
+            return response()->json([
+                'error' => 'Resource not Found'
+            ], 404);
+        }
+
         return parent::render($request, $exception);
     }
 }
